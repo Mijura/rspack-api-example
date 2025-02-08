25f92c50d92224593ea9eace67199a2a948cd989
Mijura <miodragvilotijevic@gmail.com>
2025-02-08 15:25:15 +0100

    removed build files

--- example.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RspackCompiler_1 = require("./RspackCompiler");
-const configPath = "./template/configs/rspack.prod.js";
-const compiler = new RspackCompiler_1.RspackCompiler(configPath);
-compiler.run();
-compiler.getConfig();
